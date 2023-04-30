d7e5b57b006fec1c9e712590c56278546500953d
Nam Huu Ngo <121742978+nam2k2k16fpt@users.noreply.github.com>
2023-04-30 09:59:16 +0700

    Add files via upload

---
+++ J1.S.P0069/src/j1/s/p0069/J1SP0069.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j1.s.p0069;
+
+/**
+ *
+ * @author Admin
+ */
+public class J1SP0069 {
+
+
+    public static void main(String[] args) {
+        //File reading and writing
+        
+        
+        //Fuction 1: search FileWriter, BufferedWriter
+        //Fuction 2:  search FileReader,
+    }
+    
+}
